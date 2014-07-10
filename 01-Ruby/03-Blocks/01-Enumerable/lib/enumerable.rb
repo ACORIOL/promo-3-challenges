c2454e3c2ed10b8a15c525db954869f2adbe5be1
ACORIOL <annecharlotteoriol@gmail.com>
2014-07-10 11:38:43 +0200

    plus que deux exo

--- 01-Ruby/03-Blocks/01-Enumerable/lib/enumerable.rb
+++ 01-Ruby/03-Blocks/01-Enumerable/lib/enumerable.rb
@@ -25,12 +25,15 @@ short_words(["jlfdsqbjb", "jlhgblsqjh", "ljkbsdqhj", "ghk", "hlj"], 4)
 def first_under(array, limit)
   # TODO: Return the first number from an array that is less than limit.
   #       You should use Enumerable#find
+  array.find { |x| x < limit }
 end
 
 def add_bang(array)
   # TODO: Take an array of strings and return a new array with "!" appended to each string.
   #       You should use Enumerable#map
+  array.map { |word| word + "!"}
 end
+add_bang(["arg", "agd", "fgdq"])
 
 def product(array)
   # TODO: Calculate the product of an array of numbers.

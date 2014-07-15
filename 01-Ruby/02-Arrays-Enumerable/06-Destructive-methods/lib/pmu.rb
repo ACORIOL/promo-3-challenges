059947163f1e4b95579a77f7bd6c1e3e1d64caf5
ACORIOL <annecharlotteoriol@gmail.com>
2014-07-15 17:21:00 +0200

    finalised pmu

--- 01-Ruby/02-Arrays-Enumerable/06-Destructive-methods/lib/pmu.rb
+++ 01-Ruby/02-Arrays-Enumerable/06-Destructive-methods/lib/pmu.rb
@@ -1,7 +1,11 @@
 def pmu_format!(race_array)
-  #TODO: modify the given array so that it is PMU-consistent
+#TODO: modify the given array so that it is PMU-consistent
+  i = 0
+  race_array.map! do |x|
+    i = race_array.index(x) + 1
+    "#{i}-#{x}!"
+  end
+  race_array.reverse!
 end
 
-
-
-
+pmu_format!(["horse1","horse2","horse3"])

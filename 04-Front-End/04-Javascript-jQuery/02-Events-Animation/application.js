cce2ec361b88523377506ca4ceb4e9390bca4791
ACORIOL <annecharlotteoriol@gmail.com>
2014-08-04 17:33:24 +0200

    bof

--- 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
+++ 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
@@ -3,3 +3,37 @@
   * TODO: add your code here!
   *
 */
+$(function() { // equivalent document ready
+
+$("#message-wrapper").hide().delay(500).slideDown(1000);
+
+$("#message-wrapper").on("mouseenter", function(the_event) {
+  $(this).slideDown(1000);
+});
+
+
+$("message-wrapper").on("mouseleave", (function(the_event) {
+  $(this).slideUp(1000);
+});
+
+$("body").on("mousemove", function(the_event) {
+  if (event.pageY < 50 {
+    $("#message-wrapper").slideDown(1000);
+  }
+
+  });
+
+});
+
+//EASY WAY
+
+//$("#message-wrapper").slideDown(3000);
+
+//$("message-wrapper").click(function() {
+//  $(this).slideUp(1000);
+//});
+//
+//
+//
+
+

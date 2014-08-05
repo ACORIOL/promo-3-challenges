329b776a49576a05a66abb0812caffb597ada8af
ACORIOL <annecharlotteoriol@gmail.com>
2014-08-05 16:37:43 +0200

    old ex

--- 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
+++ 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
@@ -9,22 +9,27 @@ function run_challenges() {
 
   // Ex 1. Read the content of the email input
   var email = $("#email").val();  // TODO: replace null, keep the email variable.
+  // AUTRE METHODE -> $('#email').attr('value');
 
   // Ex 2. Fill the content of the email input with your email
   var myEmail = $("#email").val("annecharlotteoriol@gmail.com");
+
   // Ex 3. Replace the email hint (next to the input) with 'This is my email now'
   //       The text should be emphasized using a <strong> tag
   $( "#email-hint" ).replaceWith( "<strong>This is my email now</strong>" );
+  // AUTRE METHODE -> $('#email-hint').html('<strong>blablabl</strong>'); SI .text ferait apparitre TOUT contenu parenthese comme texte (y compris balises)
 
   // Ex 4. Add the .blue CSS class to the table header cells (th elements)
   $('thead').css("color", "blue");
+  // AUTRE METHODE -> $("th").addCLass("blue");
 
   // Ex 5. Count the number of table body rows there are (team count!)
   var team_count = $('tbody tr').length;  // TODO: replace 0, keep the team_count variable.
 
   // Ex 6. Say there is a 15th team added to the table.
   //       Add a row at the bottom, this new team should have zero points.
-  $("table tbody").append("<tr><td>row</td></tr>");
+  $("table tbody").append("<tr><td>15</td><td>blabla</td><td>0</td></tr>");
+  // AUTRE METHODE -> $('table').append('<tr><td>'+(team_count+1)+'</td><td>blabla</td><td>0</td></tr>');
 
   // Ex 7. Write some code to sum all points given to all teams
   // var sum = 0;  // TODO: replace 0 with your sum result, keep the sum variable.
@@ -34,17 +39,16 @@ function run_challenges() {
     sum += parseInt($(value).find('td:last').text());
   });
 
-
   // Ex 8. Change the background color of all table header cells to #DDF4FF
   $('th').css('background-color', '#DDF4FF');
 
-
   // Ex 9. Translate the #results list 20px downward (animation would be great ^_^)
-  $('results-wrapper').css("padding-top","20px");
-
+  $('results-wrapper').css("padding-top", "20px");
+  // AUTRE METHODE -> $('results-wrapper').animate({'padding-top': "100px"}, 2000, "linear");
 
   // Ex 10. Remove the "Email:" label from the DOM
   $("label").remove();
+  // AUTRE METHODE (eg si il y a plusieurs labels mais qu'on ne veut en virer qu'un) -> $('label'[for='email']) A FINIR
 
 
   // Checking exercise answers. DO NOT MODIFY BELOW

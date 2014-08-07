865651171378076a9ec5462f6761092e25ca9acb
ACORIOL <annecharlotteoriol@gmail.com>
2014-08-07 10:28:32 +0200

    with reset

--- 04-Front-End/04-Javascript-jQuery/04-wagon-race/game.js
+++ 04-Front-End/04-Javascript-jQuery/04-wagon-race/game.js
@@ -1,5 +1,14 @@
-$(function(){
+function reset() {
+  console.debug('reset');
+  $('#player1_race, #player2_race')
+    .find('.active')
+      .removeClass('active')
+    .end()
+    .find('td:first-child')
+      .addClass('active');
+}
 
+$(function() {
   var input_player1 = {
     keycode: 65
   };
@@ -8,37 +17,22 @@ $(function(){
     keycode: 80
   };
 
-  $(document).keyup(function(e){
+  $(document).keyup(function(e) {
     console.log(e.which);
     if (e.which == input_player1.keycode) {
         $("#player1_race td.active").removeClass("active").next().addClass("active");
-        if ('is last ?', $("#player1_race td.active").is(':last-child') == true) {
+        if ($("#player1_race td.active").is(':last-child')) {
           alert("Congrats, Player1, You Won!");
+          reset();
         }
     } else if (e.which == input_player2.keycode) {
         $ ("#player2_race td.active").removeClass("active").next().addClass("active");
-        if ('is last ?', $("#player2_race td.active").is(':last-child') == true) {
+        if ($("#player2_race td.active").is(':last-child')) {
           alert("Congrats, Player2, You Won!");
+          reset();
         }
     }
   });
-
 });
-
-
-
-   //$ (#player1_race td.active == #player1_race td:last).alert("Congrats Player1, you won!");
-
-//});
-
-//$(function(){
-  //$ if (#player1_race td.active == #player1_race td:last) {
-    //$ alert("Congrats Player1, you won!");
- // } else if (#player2_race td.active == #player2_race td:last) {}
-   // $ alert("Congrats Player2, you won!");
-  //}
-//});
-
-
 
 
